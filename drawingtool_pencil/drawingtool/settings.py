35ea7cbddeefb424b14ffc879d17f60939fe6bad
Vishesh <visheshakavishi@gmail.com>
2022-10-22 20:02:13 +0530

    heroku preparation settings change

--- drawingtool_pencil/drawingtool/settings.py
+++ drawingtool_pencil/drawingtool/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     '/var/www/static',
 ]
+
+#Activate django-heroku
+django_heroku.settings(locals())
