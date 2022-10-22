ce4e041d799282a613f8f7bd7aec9326dc8a09be
Vishesh <visheshakavishi@gmail.com>
2022-10-22 14:45:39 +0530

    heroku hosting

---
+++ drawingtool_pencil/drawingtool/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for drawingtool project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drawingtool.settings')
+
+application = get_asgi_application()
